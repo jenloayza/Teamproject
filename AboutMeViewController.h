06cd8f258f8c4acf14231747d9e5a5627380ac46
jenloayza <jenloayza2003@yahoo.com>
2013-04-24 17:32:23 -0300

    Create AboutMeViewController.h

---
+++ AboutMeViewController.h
@@ -0,0 +1,18 @@
+//
+//  AboutMeViewController.h
+//  TeamProject
+//
+//  Created by Daniela Mora and Jenny Escobar
+//  Copyright (c) 2013 All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//View Controller which shows the company owner
+//basic information
+@interface AboutMeViewController : UIViewController
+
+//returns to main More View
+-(IBAction)backButton:(id)sender;
+
+@end
